ac228508b384392de5b246a41ac16b9fa1a33c2f
Leak1337 <beschte1337@gmx.de>
2016-08-01 17:06:31 +0200

    Gui Fixed

--- PokemonGo.RocketAPI.Console/Pokemons.designer.cs
+++ PokemonGo.RocketAPI.Console/Pokemons.designer.cs
@@ -30,64 +30,62 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pokemons));
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.PokemonListView = new System.Windows.Forms.ListView();
+            this.btnreload = new System.Windows.Forms.Button();
+            this.btnEvolve = new System.Windows.Forms.Button();
             this.btnUpgrade = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnTransfer = new System.Windows.Forms.Button();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.transferToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.powerUpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.evolveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.statusTexbox = new System.Windows.Forms.TextBox();
+            this.checkBoxreload = new System.Windows.Forms.CheckBox();
+            this.reloadsecondstextbox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.reloadtimer = new System.Windows.Forms.Timer(this.components);
             this.btnFullPowerUp = new System.Windows.Forms.Button();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listView1
+            // PokemonListView
             // 
-            this.listView1.AllowColumnReorder = true;
-            this.listView1.FullRowSelect = true;
-            this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(12, 12);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(687, 387);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
-            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            this.PokemonListView.AllowColumnReorder = true;
+            this.PokemonListView.FullRowSelect = true;
+            this.PokemonListView.GridLines = true;
+            this.PokemonListView.Location = new System.Drawing.Point(12, 12);
+            this.PokemonListView.Name = "PokemonListView";
+            this.PokemonListView.Size = new System.Drawing.Size(687, 387);
+            this.PokemonListView.TabIndex = 0;
+            this.PokemonListView.UseCompatibleStateImageBehavior = false;
+            this.PokemonListView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
             // 
-            // button1
+            // btnreload
             // 
-            this.button1.BackColor = System.Drawing.Color.Lime;
-            this.button1.Location = new System.Drawing.Point(12, 405);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Neu Laden";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnreload.BackColor = System.Drawing.Color.Lime;
+            this.btnreload.Location = new System.Drawing.Point(13, 407);
+            this.btnreload.Name = "btnreload";
+            this.btnreload.Size = new System.Drawing.Size(136, 23);
+            this.btnreload.TabIndex = 1;
+            this.btnreload.Text = "Liste neu laden";
+            this.btnreload.UseVisualStyleBackColor = false;
+            this.btnreload.Click += new System.EventHandler(this.btnReload_Click);
             // 
-            // button2
+            // btnEvolve
             // 
-            this.button2.BackColor = System.Drawing.Color.Yellow;
-            this.button2.Location = new System.Drawing.Point(13, 434);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Entwickeln";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnEvolve.BackColor = System.Drawing.Color.Yellow;
+            this.btnEvolve.Location = new System.Drawing.Point(149, 436);
+            this.btnEvolve.Name = "btnEvolve";
+            this.btnEvolve.Size = new System.Drawing.Size(135, 23);
+            this.btnEvolve.TabIndex = 2;
+            this.btnEvolve.Text = "Entwickeln";
+            this.btnEvolve.UseVisualStyleBackColor = false;
+            this.btnEvolve.Click += new System.EventHandler(this.btnEvolve_Click);
             // 
             // btnUpgrade
             // 
             this.btnUpgrade.BackColor = System.Drawing.Color.Yellow;
-            this.btnUpgrade.Location = new System.Drawing.Point(155, 434);
+            this.btnUpgrade.Location = new System.Drawing.Point(290, 435);
             this.btnUpgrade.Name = "btnUpgrade";
             this.btnUpgrade.Size = new System.Drawing.Size(135, 23);
             this.btnUpgrade.TabIndex = 3;
@@ -95,16 +93,16 @@
             this.btnUpgrade.UseVisualStyleBackColor = false;
             this.btnUpgrade.Click += new System.EventHandler(this.btnUpgrade_Click);
             // 
-            // button3
+            // btnTransfer
             // 
-            this.button3.BackColor = System.Drawing.Color.Red;
-            this.button3.Location = new System.Drawing.Point(299, 434);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(135, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Verschicken";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.btnTransfer.BackColor = System.Drawing.Color.Red;
+            this.btnTransfer.Location = new System.Drawing.Point(564, 407);
+            this.btnTransfer.Name = "btnTransfer";
+            this.btnTransfer.Size = new System.Drawing.Size(135, 23);
+            this.btnTransfer.TabIndex = 4;
+            this.btnTransfer.Text = "Verschicken";
+            this.btnTransfer.UseVisualStyleBackColor = false;
+            this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
             // 
             // contextMenuStrip1
             // 
@@ -139,67 +137,55 @@
             this.evolveToolStripMenuItem.Visible = false;
             this.evolveToolStripMenuItem.Click += new System.EventHandler(this.evolveToolStripMenuItem_Click);
             // 
-            // textBox1
+            // statusTexbox
             // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(13, 463);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(686, 20);
-            this.textBox1.TabIndex = 5;
+            this.statusTexbox.Enabled = false;
+            this.statusTexbox.Location = new System.Drawing.Point(13, 463);
+            this.statusTexbox.Name = "statusTexbox";
+            this.statusTexbox.Size = new System.Drawing.Size(686, 20);
+            this.statusTexbox.TabIndex = 5;
             // 
-            // checkBox1
+            // checkBoxreload
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(299, 409);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(101, 17);
-            this.checkBox1.TabIndex = 6;
-            this.checkBox1.Text = "Neu Laden alle:";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBoxreload.AutoSize = true;
+            this.checkBoxreload.Location = new System.Drawing.Point(235, 410);
+            this.checkBoxreload.Name = "checkBoxreload";
+            this.checkBoxreload.Size = new System.Drawing.Size(97, 17);
+            this.checkBoxreload.TabIndex = 6;
+            this.checkBoxreload.Text = "Neu laden alle:";
+            this.checkBoxreload.UseVisualStyleBackColor = true;
+            this.checkBoxreload.CheckedChanged += new System.EventHandler(this.checkboxReload_CheckedChanged);
             // 
-            // textBox2
+            // reloadsecondstextbox
             // 
-            this.textBox2.Location = new System.Drawing.Point(397, 407);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(37, 20);
-            this.textBox2.TabIndex = 7;
-            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            this.reloadsecondstextbox.Location = new System.Drawing.Point(338, 408);
+            this.reloadsecondstextbox.Name = "reloadsecondstextbox";
+            this.reloadsecondstextbox.Size = new System.Drawing.Size(37, 20);
+            this.reloadsecondstextbox.TabIndex = 7;
+            this.reloadsecondstextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.reloadsecondstextbox_KeyPress);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(440, 414);
+            this.label2.Location = new System.Drawing.Point(381, 412);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(12, 13);
             this.label2.TabIndex = 9;
             this.label2.Text = "s";
             // 
-            // timer1
+            // reloadtimer
             // 
-            this.timer1.Interval = 1000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.checkBox2.Location = new System.Drawing.Point(503, 409);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(169, 43);
-            this.checkBox2.TabIndex = 10;
-            this.checkBox2.Text = "Nur Pokemon anzeigen\r\ndie Entwickelt werden können\r\n\r\n";
-            this.checkBox2.UseVisualStyleBackColor = false;
-            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            this.reloadtimer.Interval = 1000;
+            this.reloadtimer.Tick += new System.EventHandler(this.reloadtimer_Tick);
             // 
             // btnFullPowerUp
             // 
             this.btnFullPowerUp.BackColor = System.Drawing.Color.Yellow;
-            this.btnFullPowerUp.Location = new System.Drawing.Point(155, 405);
+            this.btnFullPowerUp.Location = new System.Drawing.Point(431, 436);
             this.btnFullPowerUp.Name = "btnFullPowerUp";
             this.btnFullPowerUp.Size = new System.Drawing.Size(135, 23);
             this.btnFullPowerUp.TabIndex = 11;
-            this.btnFullPowerUp.Text = "Kompletter Power Up";
+            this.btnFullPowerUp.Text = "Full Power Up";
             this.btnFullPowerUp.UseVisualStyleBackColor = false;
             this.btnFullPowerUp.Click += new System.EventHandler(this.btnFullPowerUp_Click);
             // 
@@ -209,16 +195,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(711, 488);
             this.Controls.Add(this.btnFullPowerUp);
-            this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.reloadsecondstextbox);
+            this.Controls.Add(this.checkBoxreload);
+            this.Controls.Add(this.statusTexbox);
+            this.Controls.Add(this.btnTransfer);
             this.Controls.Add(this.btnUpgrade);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnEvolve);
+            this.Controls.Add(this.btnreload);
+            this.Controls.Add(this.PokemonListView);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -233,21 +218,20 @@
 
         #endregion
 
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView PokemonListView;
+        private System.Windows.Forms.Button btnreload;
+        private System.Windows.Forms.Button btnEvolve;
         private System.Windows.Forms.Button btnUpgrade;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnTransfer;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem transferToolStripMenuItem;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox statusTexbox;
         private System.Windows.Forms.ToolStripMenuItem powerUpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem evolveToolStripMenuItem;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBoxreload;
+        private System.Windows.Forms.TextBox reloadsecondstextbox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Timer reloadtimer;
         private System.Windows.Forms.Button btnFullPowerUp;
     }
 }
